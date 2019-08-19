aae005dc1a30f8ca197c83a31c8c08e97372c5e1
Logon Aluno <logonaluno@local.com>
2019-08-19 11:54:47 -0300

    Atualizações do server

---
+++ 02-Fiap.Web.AspNet/02-Fiap.Web.AspNet/Controllers/FormularioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _02_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_Fiap.Web.AspNet.Controllers
+{
+    public class FormularioController : Controller
+    {
+
+        private static IList<Pessoa> _lista = new List<Pessoa>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Pessoa pessoa)
+        {
+            ViewData["name"] = pessoa.Name;
+            ViewData["dataNascimento"] = pessoa.DataNascimento ;
+            Console.WriteLine("QUEM É " + pessoa.Email);
+            ViewData["email"] = pessoa.Email;
+            ViewBag.usuario = pessoa.Name;
+            TempData["message"] = "Usuário Cadastrado";
+            _lista.Add(pessoa);
+            return View(pessoa);
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return View(_lista);
+        }
+    }
+}
---
+++ 02-Fiap.Web.AspNet/02-Fiap.Web.AspNet/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_Fiap.Web.AspNet.Models
+{
+    public class Pessoa
+    {
+
+        public String Name { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public String Email { get; set; }
+
+    }
+}
